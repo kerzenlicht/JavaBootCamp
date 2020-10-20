ec32a7ff4fcd2343241e1eaa99df53cbb882cfbf
Karina Junga <karina_11c@inbox.lv>
2020-10-20 09:22:19 +0300

    :Activity Day 1

---
+++ SEF/src/sef/module2/sample/HelloWorld2.java
@@ -0,0 +1,10 @@
+package sef.module2.sample;
+
+public class HelloWorld2 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World! - Karin");
+
+	}
+
+}
--- SEF/src/sef/module3/activity/VariableAssignmentActivity.java
+++ SEF/src/sef/module3/activity/VariableAssignmentActivity.java
@@ -12,12 +12,17 @@ public class VariableAssignmentActivity {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	public static void main (String args[]) {
+		 String abc = "Hi!";
+		abc="xyz";
+			System.out.println(abc);
+			}
+		
+}	
+		
 		
 		// 1- Declare a variable of type int and assign it default value.
 		// 2- Update the value 
 		// 3- Print updated value to the console
 
-	}
 
-}
